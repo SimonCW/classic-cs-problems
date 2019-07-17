de26c1d383b8bf3ae0338dc4af49e1f71b3a86b0
Simon Weiss <simon.weiss@alexanderthamm.com>
2019-07-17 16:17:26 +0200

    Test

--- hanoi.py
+++ hanoi.py
@@ -24,8 +24,23 @@ tower_c: Stack[int] = Stack()
 for i in range(1, num_discs + 1):
     tower_a.push(i)
 
-print(tower_a)
-
 
 def hanoi(begin: Stack[int], end: Stack[int], temp: Stack[int], n: int) -> None:
-    pass
+    if n == 1:
+        end.push(begin.pop())
+    else:
+        hanoi(begin, temp, end, n - 1)
+        hanoi(begin, end, temp, 1)
+        hanoi(temp, end, begin, n - 1)
+
+
+if __name__ == "__main__":
+    print("-------Before Game-------")
+    print(tower_a)
+    print(tower_b)
+    print(tower_c)
+    hanoi(tower_a, tower_c, tower_b, num_discs)
+    print("-------After Game------")
+    print(tower_a)
+    print(tower_b)
+    print(tower_c)
