ae296d68f7cb073d131c7bf25d1e448195eb9ee6
Simon Weiss <simon.weiss@alexanderthamm.com>
2020-07-17 00:33:53 +0200

    Implement stack and node

--- src/generic_search.py
+++ src/generic_search.py
@@ -56,5 +56,40 @@ def binary_contains(sequence: Sequence[C], key: C) -> bool:
     return False
 
 
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return not self._container
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.pop()
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
+class Node(Generic[T]):
+    def __init__(
+        self,
+        state: T,
+        parent: Optional[Node],
+        cost: float = 0.0,
+        heuristic: float = 0.0,
+    ) -> None:
+        self.state: T = state
+        self.parent: Optional[Node] = parent
+        self.cost: float = cost
+        self.heuristic: float = heuristic
+
+        def __lt__(self, other: Node) -> bool:
+            return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+
+
 if __name__ == "__main__":
     print(binary_contains(sorted(["a", "c", "b", "f", "h"]), "c"))
